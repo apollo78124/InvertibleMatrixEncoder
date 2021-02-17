64e95fe205682b132d11ca4c959d0fc15f3e4ae7
apollo78124 <98apollo@naver.com>
2021-02-17 02:47:56 -0800

    Encoding works!

--- src/EncodeDecode.java
+++ src/EncodeDecode.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class EncodeDecode {
 	MatrixMultiplier calculator;
@@ -5,7 +6,7 @@ public class EncodeDecode {
 		calculator = new MatrixMultiplier();
 	}
 	
-	public int[] stringToMatrix(String input) {
+	public int[] stringToNumericalArray(String input) {
 		int[] output = new int[input.length()];
 		char character = input.charAt(0); // This gives the character 'a'
 		int ascii = (int) character;
@@ -24,28 +25,58 @@ public class EncodeDecode {
 			}
 			
 		}
+		System.out.println("\nEnd of Numeric Array");
 		return output;
 	}
 	
-	public String encode(String input) {
+	public ArrayList<int[][]> vectorToMatrix(int[] input) {
+		
+		ArrayList<int[][]> output = new ArrayList<int[][]>();
+		int[][] diGraph = { {0}, {0} };
+		for (int i = 0; i < input.length; i++) {
+			if (i % 2 ==  0) {
+				if (i + 1 > input.length - 1) {
+					diGraph = new int[][]{ {input[i]}, {0} };
+				} else {
+					diGraph = new int[][]{ {input[i]}, {input[i + 1]} };
+				}
+				output.add(diGraph);
+			}
+		}
+		
+		return output;
+	}
+	
+	public String encode(String input, int[][] K) {
 		String cipherText = "";
-		int r1 = 2, c1 = 3;
-        int r2 = 3, c2 = 2;
-        int[][] firstMatrix = { {3, -2, 5}, {3, 0, 4} };
-        int[][] secondMatrix = { {16}, {12} };
+		int r1 = 0, c1 = 0;
+        int r2 = 0, c2 = 0;
         
-        int[][] K = { {3, 5}, {1, 6} };
+        int[] numericArray = stringToNumericalArray(input);
+        ArrayList<int[][]> diGraphList = vectorToMatrix(numericArray);
+        ArrayList<int[][]> transformedDiGraph = new ArrayList<int[][]>();
         
-        r1 = K.length;
-        c1 = K[0].length;
-        c2 = secondMatrix[0].length;
-        // Mutliplying Two matrices
-        int[][] product = calculator.multiplyMatrices(K, secondMatrix, r1, c1, c2);
-
-        // Displaying the result
-        calculator.displayMatrix(product);
+		for (int[][] diGraph : diGraphList) {
+			r1 = K.length;
+	        c1 = K[0].length;
+	        c2 = diGraph[0].length;
+	        int[][] transformed = calculator.multiplyMatrices(K, diGraph, r1, c1, c2);
+	        transformedDiGraph.add(transformed);
+		}
 		
-		
+		for (int[][] diGraph : transformedDiGraph) {
+			for(int[] row : diGraph) {
+	            for (int column : row) {
+	            	column  = column % 27;
+	            	if (column < 1)
+	            		cipherText = cipherText + "-";
+	            	else 
+	            		cipherText = cipherText + (char) (column + 64);
+	            }
+	        }
+		}
+		System.out.println("Encoded Cipher Text: ");
+		System.out.println(cipherText);
 		return cipherText;
 	}
 	
--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
 
 public class Runner {
 	
     
 	public static void main(String[] args) {
-		
+		MatrixMultiplier calculator;
+		calculator = new MatrixMultiplier();
 		EncodeDecode encoder = new EncodeDecode();
-		encoder.stringToMatrix("PLEASE USE THE OTHER DOOR");
+		int[][] K = new int[][]{ {3, 5}, {1, 6} };
+		encoder.encode("PLEASE USE THE OTHER DOOR", K);
     }
 }
